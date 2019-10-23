b7e1b5f296f738151451e87b882384735dbc6266
glicheur123 <46579615+glicheur123@users.noreply.github.com>
2019-10-22 21:05:19 -0400

    J'approuve

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,11 +7,25 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+
+  private Joystick stick;
+
+  public OI()
+  {
+    stick = new Joystick(0);
+  }
+
+  public Joystick getJoystick()
+  {
+    return stick;
+  }
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
